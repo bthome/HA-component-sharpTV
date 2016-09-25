9eac21f1fee11b94ade573df92f248f8fba53728
Brian <bthome@users.noreply.github.com>
2016-09-25 11:40:57 -0500

    Update sharptv.py
    Refactored component.  TV has "timeouts", put a loop in to retry before throwing an error or warning.

--- sharptv.py
+++ sharptv.py
@@ -6,11 +6,12 @@ https://home-assistant.io/components/media_player.sharptv/
 """
 import logging
 import socket
+import time
 
 from homeassistant.components.media_player import (
     DOMAIN, SUPPORT_NEXT_TRACK, SUPPORT_PREVIOUS_TRACK,
     SUPPORT_TURN_OFF, SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_STEP,
-    SUPPORT_SELECT_SOURCE, MediaPlayerDevice)
+    SUPPORT_PAUSE, SUPPORT_SELECT_SOURCE, MediaPlayerDevice)
 from homeassistant.const import (
     CONF_HOST, CONF_NAME, STATE_OFF, STATE_ON, STATE_UNKNOWN)
 from homeassistant.helpers import validate_config
@@ -60,62 +61,59 @@ class SharpTVDevice(MediaPlayerDevice):
     def __init__(self, name, config):
         """Initialize the sharp device."""
         from sharp_aquos_rc import TV
-        # Save a reference to the imported class
-        self._remote_class = TV
+        self._device = TV(config["host"], 
+                          config["port"], 
+                          str(config["user"]), 
+                          str(config["password"]))
+
         self._name = name
         # Assume that the TV is not muted
         self._muted = False
         self._state = STATE_UNKNOWN
-        self._remote = None
         self._current_source = None
-        self._source_list = {'TV /Antenna': 0,
-                             'HDMI_IN_1': 1,
-                             'HDMI_IN_2': 2,
-                             'HDMI_IN_3': 3,
-                             'HDMI_IN_4': 4,
-                             'COMPONENT IN': 5,
-                             'VIDEO_IN_1': 6,
-                             'VIDEO_IN_2': 7,
-                             'PC_IN': 8};
-        self._config = config
+        self._source_list = {'0': 'TV /Antenna',
+                             '1': 'HDMI_IN_1',
+                             '2': 'HDMI_IN_2',
+                             '3': 'HDMI_IN_3',
+                             '4': 'HDMI_IN_4',
+                             '5': 'COMPONENT IN',
+                             '6': 'VIDEO_IN_1',
+                             '7': 'VIDEO_IN_2',
+                             '8': 'PC_IN'};
+        self.update()
 
     def update(self):
         """Retrieve the latest data."""
-        # Send an empty key to see if we are still connected
-        return self.send_key('KEY_POWER')
-
-    def get_remote(self):
-        """Create or return a remote control instance."""
-        if self._remote is None:
-            # We need to create a new instance to reconnect.
-            self._remote = self._remote_class(self._config["host"], self._config["port"], str(self._config["user"]), str(self._config["password"]))
-
-        return self._remote
-
-    def send_key(self, key):
-        """Validates TV power status."""
-        """FUTURE: Send all possible keys to the tv and handles exceptions."""
-        try:
-            p = self.get_remote().power()
+        for i in range(5):
+            try:
+                p = self._device.power()
             
-            if (p == 1): self._state = STATE_ON 
-            elif (p == 0): self._state = STATE_OFF
-            else: _LOGGER.debug('Found Unknown State [%s]', p)
-            
-            if (self.get_remote().mute() == 1): self._muted = True 
-            else: self._muted = False
+                if (p == 1): 
+                    self._state = STATE_ON
 
-            i = self.get_remote().input()
-            for key in self._source_list:
-               if self._source_list.get(key) == i:
-                  self._current_source = key
-            
-            _LOGGER.debug('Found Current Source [%s] [%d]', self._current_source, i)
+                    if (self._device.mute() == 1): self._muted = True 
+                    else: self._muted = False
 
-        except (socket.timeout, TimeoutError, OSError):
-            _LOGGER.info('Exception: Timeout.  Current state [%s]', self._state) 
-        except BaseException as e:
-            _LOGGER.info('Exception: Unknown.  Current state [%s] Error [%s]', self._state, repr(e)) 
+                    inp = self._device.input()
+                    self._current_source = self._source_list.get(inp)
+
+                    _LOGGER.debug('Found Current Source [%s] [%d]', self._current_source, inp)
+            
+                elif (p == 0): self._state = STATE_OFF
+                else: _LOGGER.debug('Found Unknown State [%s]', p)
+
+                break
+            except (socket.timeout, TimeoutError, OSError) as e:
+            #except TimeoutError as e:
+                time.sleep(2)
+                if (i >= 4):
+                    _LOGGER.warning('Exception: Timeout.  Current state [%s] Error [%s]', self._state, repr(e)) 
+                pass
+            except BaseException as e:
+                time.sleep(2)
+                if (i >= 4):
+                    _LOGGER.warning('Exception: Unknown.  Current state [%s] Error [%s]', self._state, repr(e)) 
+                pass
 
         return True
 
@@ -142,51 +140,57 @@ class SharpTVDevice(MediaPlayerDevice):
     @property
     def source_list(self):
         """List of available input sources."""
-        return sorted(list(self._source_list.keys()))
+        return sorted(list(self._source_list.values()))
 
     @property
     def supported_media_commands(self):
         """Flag of media commands that are supported."""
         return SUPPORT_SHARPTV
 
-    def turn_off(self):
-        """Turn off media player."""
-        self.get_remote().power_on_command_settings(2)
-        self.get_remote().power(0) 
-        self.send_key('KEY_POWER')
-
     def volume_up(self):
         """Volume up the media player."""
-        self.get_remote().volume(self.get_remote().volume() + 1)
+        self._device.volume(self._device.volume() + 1)
 
     def volume_down(self):
         """Volume down media player."""
-        self.get_remote().volume(self.get_remote().volume() - 1)
+        self._device.volume(self._device.volume() - 1)
 
     def mute_volume(self, mute):
         """Send mute command."""
-        self.get_remote().mute(0)
-        if (self.get_remote().mute() == 1): self._muted = True 
+        self._device.mute(0)
+        if (self._device.mute() == 1): self._muted = True 
         else: self._muted = False
 
     def media_next_track(self):
         """Send next track command."""
-        self.get_remote().channel_up()
+        self._device.channel_up()
 
     def media_previous_track(self):
         """Send the previous track command."""
-        self.get_remote().channel_down()
+        self._device.channel_down()
+
+    def media_pause(self):
+        """Send the previous track command."""
+        self._device.sleep(1)
 
     def turn_on(self):
         """Turn the media player on."""
-        self.get_remote().power_on_command_settings(2)
-        self.get_remote().power(1) 
-        self.send_key('KEY_POWER')
+        self._device.power_on_command_settings(2)
+        self._device.power(1) 
+        self.update()
+
+    def turn_off(self):
+        """Turn off media player."""
+        self._device.power_on_command_settings(2)
+        self._device.power(0) 
+        self.update()
 
     def select_source(self, source):
         """Select input source."""
-        i = self._source_list.get(source)
-        _LOGGER.info('Source Selection: New Source [%s] Input [%d]', source, i) 
-        self.get_remote().input(i)
-        self.send_key('KEY_SOURCE')
+        for k, v in self._source_list.items():
+            if v == source:
+                self._device.input(k)
+                _LOGGER.info('Source Selection: New Source [%s] Input [%s]', v, k) 
+
+        self.update()
 
